4619b09cb09389863369025604709c94ce7350c7
teckenrod <treyeck2@gmail.com>
2024-01-31 18:59:49 -0600

    Added settings view

--- RocketSync/RocketSync/Views/SettingsView.swift
+++ RocketSync/RocketSync/Views/SettingsView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct SettingsView: View {
     @State private var darkMode = false
+    @State private var notifications = false
     
     var body: some View {
         VStack {
@@ -40,12 +41,16 @@ struct SettingsView: View {
             
             HStack {
                 Image(systemName: "bell.badge.fill")
-                Toggle("Notifications", isOn: $darkMode)
+                Toggle("Notifications", isOn: $notifications)
             }.padding(.all)
             
             HStack {
                 Image(systemName: "sun.max.fill")
                 Toggle("Dark Mode", isOn: $darkMode)
+            }.padding(.all)
+            
+            HStack {
+                
             }.padding(.all)
         }
         Spacer()
