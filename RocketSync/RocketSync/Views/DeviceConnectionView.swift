14fe0fd125df9f1afe6912ccaf67955ce8822e79
teckenrod <treyeck2@gmail.com>
2024-01-31 19:00:02 -0600

    Added connection view, still needs work

---
+++ RocketSync/RocketSync/Views/DeviceConnectionView.swift
@@ -0,0 +1,20 @@
+//
+//  DeviceConnectionView.swift
+//  RocketSync
+//
+//  Created by Trey Eckenrod on 1/28/24.
+//
+
+import SwiftUI
+import CoreBluetooth
+
+struct DeviceConnectionView: View {
+    
+    var body: some View {
+        Text("Device Connection Screen")
+    }
+}
+
+#Preview {
+    DeviceConnectionView()
+}
