8199b300a79601f13a394df38e7a8cfb78bc414f
Charles Wilmot II <chazwilmot@gmail.com>
2024-03-12 14:51:42 -0500

    Update BluetoothManager.swift
    Added class

--- RocketSync/RocketSync/Controller/BluetoothManager.swift
+++ RocketSync/RocketSync/Controller/BluetoothManager.swift
@@ -6,3 +6,10 @@
 
 import Foundation
 import CoreBluetooth
+
+class BLEManager: NSObject, ObservableObject, CBCentralManagerDelegate, CBPeripheralDelegate {
+    var centralManager: CBCentralManager!
+    @Published var discoveredPeripherals = [CBPeripheral]()
+    @Published var receivedData = ""
+
+}
