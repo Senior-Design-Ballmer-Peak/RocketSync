0b357ce42c29f26cbeaff42593b861d252ba8fb7
teckenrod <treyeck2@gmail.com>
2024-03-18 16:34:31 -0500

    added post options enum

---
+++ RocketSync/RocketSync/Enums/PostOptions.swift
@@ -0,0 +1,15 @@
+//
+//  PostOptions.swift
+//  RocketSync
+//
+//  Created by Trey Eckenrod on 3/17/24.
+//
+
+import Foundation
+
+enum PostOptions {
+    case question
+    case post
+    case design
+    case launch
+}
