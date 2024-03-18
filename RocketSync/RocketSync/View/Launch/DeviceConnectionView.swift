bbb5efb7208abbeb34efec6fb33eff8ceee8557f
teckenrod <treyeck2@gmail.com>
2024-03-18 16:26:41 -0500

    Updated device connection view

--- RocketSync/RocketSync/View/Launch/DeviceConnectionView.swift
+++ RocketSync/RocketSync/View/Launch/DeviceConnectionView.swift
@@ -6,80 +6,96 @@
 //
 
 import SwiftUI
+import CoreBluetooth
 
 struct DeviceConnectionView: View {
+    @ObservedObject var bluetoothManager = BluetoothController()
+    @State private var selectedPeripheral: CBPeripheral?
     
     var body: some View {
-        Text("Device Connection View")
-            .toolbar {
-                ToolbarItem(placement: .topBarLeading) {
-                    HStack {
-                        Image(systemName: "location.north")
-                            .foregroundColor(Color("TextColor"))
-                            
-                        Text("RocketSync")
-                            .font(.title)
-                            .fontWidth(.expanded)
-                            .foregroundColor(Color("TextColor"))
+        NavigationStack {
+            List {
+                ForEach(bluetoothManager.discoveredPeripherals, id: \.identifier) { peripheral in
+                    NavigationLink(value: peripheral) {
+                        Text(peripheral.name ?? "Unknown")
                     }
                 }
-                
-                ToolbarItem(placement: .topBarTrailing) {
-                    NavigationLink(destination: SettingsView()) {
-                        Image(systemName: "gear")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .foregroundColor(Color("TextColor"))
-                    }
-                }
-                
-                ToolbarItemGroup(placement: .bottomBar) {
-                    
-                    Spacer()
-                    
-                    NavigationLink(destination: PostsView()) {
-                        Image(systemName: "aqi.medium")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 30, height: 30)
-                            .foregroundColor(Color("TextColor"))
-                            .opacity(0.4)
-                    }
-                    
-                    Spacer()
-                    
-                    NavigationLink(destination: DistanceMeasureView()) {
-                        Image(systemName: "scope")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 30, height: 30)
-                            .foregroundColor(Color("TextColor"))
-                            .opacity(0.4)
-                    }
+            }
+            .navigationTitle("Discovered Devices")
+            .navigationDestination(for: CBPeripheral.self) { peripheral in
+                DeviceDetailView(bluetoothManager: bluetoothManager, peripheral: peripheral)
+            }
+        }
+        
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                HStack {
+                    Image(systemName: "location.north")
+                        .foregroundColor(Color("TextColor"))
                         
-                    Spacer()
-                    
-                    Image(systemName: "location.north.line.fill")
+                    Text("RocketSync")
+                        .font(.title)
+                        .fontWidth(.expanded)
+                        .foregroundColor(Color("TextColor"))
+                }
+            }
+            
+            ToolbarItem(placement: .topBarTrailing) {
+                NavigationLink(destination: SettingsView()) {
+                    Image(systemName: "gear")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .foregroundColor(Color("TextColor"))
+                }
+            }
+            
+            ToolbarItemGroup(placement: .bottomBar) {
+                
+                Spacer()
+                
+                NavigationLink(destination: PostsView()) {
+                    Image(systemName: "aqi.medium")
                         .resizable()
                         .aspectRatio(contentMode: .fit)
                         .frame(width: 30, height: 30)
                         .foregroundColor(Color("TextColor"))
-                        .opacity(1)
-                    
-                    Spacer()
-                    
-                    NavigationLink(destination: ProfileView()) {
-                        Image(systemName: "person.fill")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 30, height: 30)
-                            .foregroundColor(Color("TextColor"))
-                            .opacity(0.4)
-                    }
-                    
-                    Spacer()
+                        .opacity(0.4)
                 }
+                
+                Spacer()
+                
+                NavigationLink(destination: DistanceMeasureView()) {
+                    Image(systemName: "scope")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 30, height: 30)
+                        .foregroundColor(Color("TextColor"))
+                        .opacity(0.4)
+                }
+                    
+                Spacer()
+                
+                Image(systemName: "location.north.line.fill")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 30, height: 30)
+                    .foregroundColor(Color("TextColor"))
+                    .opacity(1)
+                
+                Spacer()
+                
+                NavigationLink(destination: ProfileView()) {
+                    Image(systemName: "person.fill")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 30, height: 30)
+                        .foregroundColor(Color("TextColor"))
+                        .opacity(0.4)
+                }
+                
+                Spacer()
             }
+        }
         .tint(Color("TextColor"))
         .navigationBarBackButtonHidden(true)
     }
