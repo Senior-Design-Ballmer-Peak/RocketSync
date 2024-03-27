ea6898b13bf667917c945aefe1d32a63494f387b
teckenrod <treyeck2@gmail.com>
2024-03-27 18:31:31 -0500

    updated post detail view to show likes and comments

--- RocketSync/RocketSync/View/Posts/PostDetailView.swift
+++ RocketSync/RocketSync/View/Posts/PostDetailView.swift
@@ -8,7 +8,9 @@
 import SwiftUI
 
 struct PostDetailView: View {
+    @StateObject private var postController = PostsController()
     var post: Post
+    var expanded = false
     
     var body: some View {
         VStack(alignment: .leading, spacing: 8) {
@@ -49,43 +51,70 @@ struct PostDetailView: View {
                 .foregroundColor(Color("TextColor"))
                 .padding(.horizontal)
             
-            Divider()
-            
-            HStack {
-                Spacer()
-                Button(action: {
-                    // Handle like action
-                }) {
-                    Image(systemName: "heart")
-                        .resizable()
-                        .frame(width: 20, height: 20)
+            if expanded {
+                
+                Divider()
+                
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        // Handle like action
+                    }) {
+                        Image(systemName: "heart")
+                            .resizable()
+                            .frame(width: 20, height: 20)
+                            .foregroundColor(Color("TextColor"))
+                    }
+                    Text("\(post.likes)")
                         .foregroundColor(Color("TextColor"))
-                }
-                Text("0")
-                    .foregroundColor(Color("TextColor"))
-                
-                Spacer()
-                
-                Button(action: {
-                    // Handle comment action
-                }) {
-                    Image(systemName: "bubble.left")
-                        .resizable()
-                        .frame(width: 20, height: 20)
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                        // Handle comment action
+                    }) {
+                        Image(systemName: "bubble.left")
+                            .resizable()
+                            .frame(width: 20, height: 20)
+                            .foregroundColor(Color("TextColor"))
+                    }
+                    
+                    Text("\(post.commentText.count)")
                         .foregroundColor(Color("TextColor"))
-                }
-                
-                Text("0")
-                    .foregroundColor(Color("TextColor"))
 
+                    Spacer()
+                }
+                .padding(.horizontal, 16)
+                
+                Divider()
+            
+                VStack {
+                    ForEach(Array(zip(post.commentText, post.commentUsers)), id: \.0) { pair in
+                        HStack {
+                            Image(systemName: "person.circle")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 20, height: 20, alignment: .center)
+                                .foregroundColor(Color("TextColor"))
+                            Text(pair.1)
+                                .foregroundColor(Color("TextColor"))
+                            
+                            Spacer()
+                            
+                            Text(pair.0)
+                                .foregroundColor(Color("TextColor"))
+                        }
+                        .padding(.horizontal)
+                    }
+                }
+                
                 Spacer()
             }
-            .padding(.horizontal, 16)
         }
         .padding(.vertical, 8)
     }
 }
 
 #Preview {
-    PostDetailView(post: Post(id: "", title: "Preview", type: "Design", user: "teckenrod"))
+    PostDetailView(post: Post(id: "", title: "Preview", type: "Design", user: "teckenrod", likes: 3, commentText: ["nice", "looks good"], commentUsers: ["teckenrod", "tpawlenty"]), expanded: true)
 }
