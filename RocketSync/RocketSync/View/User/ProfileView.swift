abb564e7ea3dc5d29c255d45f81ae1c28ddeefac
teckenrod <treyeck2@gmail.com>
2024-04-07 00:12:11 -0500

    profile view works pretty well

--- RocketSync/RocketSync/View/User/ProfileView.swift
+++ RocketSync/RocketSync/View/User/ProfileView.swift
@@ -9,171 +9,168 @@ import SwiftUI
 import FirebaseAuth
 
 struct ProfileView: View {
-    @EnvironmentObject var postController: PostsController
+    var postController: PostsController
     @State private var posts: [Post] = []
     @State private var isLoading = false
     @State var selection : Int = 0
     
     var body: some View {
-        VStack {
-            if selection == 0 {
-                HStack {
-                    Image(systemName: "person.circle")
-                        .resizable()
-                        .aspectRatio(1, contentMode: .fit)
-                        .frame(width: 150)
-                        .padding(.all)
-                        .foregroundColor(Color("TextColor"))
-                    
-                    VStack {
-                        Text(Auth.auth().currentUser?.displayName ?? "Name")
-                            .fontWeight(.bold)
-                            .foregroundColor(Color("TextColor"))
-                        Text(Auth.auth().currentUser?.email ?? "email")
-                            .fontWeight(.light)
+        NavigationStack {
+            VStack {
+                if selection == 0 {
+                    HStack {
+                        Image(systemName: "person.circle")
+                            .resizable()
+                            .aspectRatio(1, contentMode: .fit)
+                            .frame(width: 150)
+                            .padding(.all)
                             .foregroundColor(Color("TextColor"))
                         
-                        NavigationLink(destination: CreatePostView(), label: {
-                            Text("Create Post")
-                                .padding(.all)
-                                .foregroundStyle(.tint)
-                                .background(RoundedRectangle(cornerRadius: 20, style: .continuous).stroke(.tint, lineWidth: 2))
+                        VStack {
+                            Text(Auth.auth().currentUser?.displayName ?? "Name")
+                                .fontWeight(.bold)
                                 .foregroundColor(Color("TextColor"))
-                        })
+                            Text(Auth.auth().currentUser?.email ?? "email")
+                                .fontWeight(.light)
+                                .foregroundColor(Color("TextColor"))
+                        }
+                        
+                        Spacer()
                     }
+                }
+                
+                HStack {
+                    Spacer()
+                    
+                    Button(action: {
+                        self.selection = 0
+                    }, label: {
+                        Image(systemName: "figure.stand")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 25, height: 30, alignment: .center)
+                            .foregroundColor(Color("TextColor"))
+                            .opacity(selection == 0 ? 1 : 0.4)
+                    })
                     
                     Spacer()
-                }
-            }
-            
-            HStack {
-                Spacer()
+                    
+                    Button(action: {
+                        self.selection = 1
+                    }, label: {
+                        Image(systemName: "text.bubble")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 25, height: 30, alignment: .center)
+                            .foregroundColor(Color("TextColor"))
+                            .opacity(selection == 1 ? 1 : 0.4)
+                    })
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                        self.selection = 2
+                    }, label: {
+                        Image(systemName: "person.fill.questionmark")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 30, height: 30, alignment: .center)
+                            .foregroundColor(Color("TextColor"))
+                            .opacity(selection == 2 ? 1 : 0.4)
+                    })
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                        self.selection = 3
+                    }, label: {
+                        Image(systemName: "location.north.line.fill")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 30, height: 30, alignment: .center)
+                            .foregroundColor(Color("TextColor"))
+                            .opacity(selection == 3 ? 1 : 0.4)
+                    })
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                        self.selection = 4
+                    }, label: {
+                        Image(systemName: "scale.3d")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 30, height: 30, alignment: .center)
+                            .foregroundColor(Color("TextColor"))
+                            .opacity(selection == 4 ? 1 : 0.4)
+                    })
+                    
+                    Spacer()
+                    
+                }.padding(.all)
                 
-                Button(action: {
-                    self.selection = 0
-                }, label: {
-                    Image(systemName: "figure.stand")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 25, height: 30, alignment: .center)
-                        .foregroundColor(Color("TextColor"))
-                        .opacity(selection == 0 ? 1 : 0.4)
-                })
-                
-                Spacer()
-                
-                Button(action: {
-                    self.selection = 1
-                }, label: {
-                    Image(systemName: "text.bubble")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 25, height: 30, alignment: .center)
-                        .foregroundColor(Color("TextColor"))
-                        .opacity(selection == 1 ? 1 : 0.4)
-                })
-                
-                Spacer()
-                
-                Button(action: {
-                    self.selection = 2
-                }, label: {
-                    Image(systemName: "person.fill.questionmark")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 30, height: 30, alignment: .center)
-                        .foregroundColor(Color("TextColor"))
-                        .opacity(selection == 2 ? 1 : 0.4)
-                })
-                
-                Spacer()
-                
-                Button(action: {
-                    self.selection = 3
-                }, label: {
-                    Image(systemName: "location.north.line.fill")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 30, height: 30, alignment: .center)
-                        .foregroundColor(Color("TextColor"))
-                        .opacity(selection == 3 ? 1 : 0.4)
-                })
-                
-                Spacer()
-                
-                Button(action: {
-                    self.selection = 4
-                }, label: {
-                    Image(systemName: "scale.3d")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 30, height: 30, alignment: .center)
-                        .foregroundColor(Color("TextColor"))
-                        .opacity(selection == 4 ? 1 : 0.4)
-                })
-                
-                Spacer()
-                
-            }.padding(.all)
-            
-            switch selection {
-            case 0:
-                List {
-                    ForEach(postController.getUserPosts()) { post in
-                        Section {
-                            PostDetailView(post: post, expanded: false)
+                switch selection {
+                case 0:
+                    List {
+                        ForEach(postController.getUserPosts()) { post in
+                            Section {
+                                PostDetailView(post: post, expanded: false)
+                            }
                         }
                     }
-                }
-
-            case 1:
-                List {
-                    ForEach(postController.getUserPosts(type: "post")) { post in
-                        Section {
-                            PostDetailView(post: post, expanded: false)
+                    
+                case 1:
+                    List {
+                        ForEach(postController.getUserPosts(type: "post")) { post in
+                            Section {
+                                PostDetailView(post: post, expanded: false)
+                            }
                         }
                     }
+                    
+                case 2:
+                    List {
+                        ForEach(postController.getUserPosts(type: "question")) { post in
+                            Section {
+                                PostDetailView(post: post, expanded: false)
+                            }
+                        }
+                    }
+                    
+                case 3:
+                    List {
+                        ForEach(postController.getUserPosts(type: "launches")) { post in
+                            Section {
+                                PostDetailView(post: post, expanded: false)
+                            }
+                        }
+                    }
+                    
+                case 4:
+                    List {
+                        ForEach(postController.getUserPosts(type: "designs")) { post in
+                            Section {
+                                PostDetailView(post: post, expanded: false)
+                            }
+                        }
+                    }
+                    
+                default:
+                    Spacer()
                 }
                 
-            case 2:
-                List {
-                    ForEach(postController.getUserPosts(type: "question")) { post in
-                        Section {
-                            PostDetailView(post: post, expanded: false)
-                        }
-                    }
-                }
-
-            case 3:
-                List {
-                    ForEach(postController.getUserPosts(type: "launches")) { post in
-                        Section {
-                            PostDetailView(post: post, expanded: false)
-                        }
-                    }
-                }
-
-            case 4:
-                List {
-                    ForEach(postController.getUserPosts(type: "designs")) { post in
-                        Section {
-                            PostDetailView(post: post, expanded: false)
-                        }
-                    }
-                }
-    
-            default:
-                Spacer()
+                NavigationLink(destination: CreatePostView(), label: {
+                    Text("Create Post")
+                        .padding(.all)
+                        .foregroundStyle(.tint)
+                        .background(RoundedRectangle(cornerRadius: 20, style: .continuous).stroke(.tint, lineWidth: 2))
+                        .foregroundColor(Color("TextColor"))
+                })
             }
         }
-    }
-    
-    func fetchPosts() {
-        isLoading = true
-        self.posts = postController.getUserPosts()
     }
 }
 
 #Preview {
-    ProfileView()
+    ProfileView(postController: PostsController())
 }
