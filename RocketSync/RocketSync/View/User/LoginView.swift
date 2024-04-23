529dd2a2cbc814affffc6132a4bceb327cc89abc
teckenrod <treyeck2@gmail.com>
2024-04-23 08:52:06 -0500

    Updated UI for login view

--- RocketSync/RocketSync/View/User/LoginView.swift
+++ RocketSync/RocketSync/View/User/LoginView.swift
@@ -21,17 +21,42 @@ struct LoginView: View {
     
     var body: some View {
         VStack {
+            ZStack {
+                Image("Icon")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 250, height: 250)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color("TextColor"), lineWidth: 5))
+            }
+            
+            Text("RocketSync")
+                .font(.title)
+                .fontWidth(.expanded)
+                .foregroundColor(Color("TextColor"))
+                .padding(.all)
+            
+            Spacer()
+            
             TextField("Email Address", text: $email)
                 .textContentType(.emailAddress)
                 .keyboardType(.emailAddress)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding(.horizontal)
                 .focused($emailIsFocused)
                 .submitLabel(.next)
             
+            
             SecureField("Password", text: $password)
                 .submitLabel(.next)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
                 .focused($passwordIsFocused)
+                .padding(.horizontal)
+                .padding(.bottom, 20)
                 .autocorrectionDisabled()
                 .textInputAutocapitalization(.never)
+                .submitLabel(.done)
+
                 
             Button(action: {
                 showAuthLoader = true
@@ -52,8 +77,11 @@ struct LoginView: View {
                     .alert(isPresented: $showInvalidPWAlert) {
                         Alert(title: Text("Email or Password is incorrect"))
                     }
-                    .frame(width: 200, height: 40)
-                    .background(RoundedRectangle(cornerRadius: 20).fill(Color(.background)))
+                    .frame(maxWidth: .infinity, minHeight: 50)
+                    .background(RoundedRectangle(cornerRadius: 25).fill(Color.blue))
+                    .foregroundStyle(Color.white)
+                    .padding(.horizontal)
+                    
             })
             
             Button(action: {
@@ -75,8 +103,10 @@ struct LoginView: View {
                     .alert(isPresented: $showInvalidPWAlert) {
                         Alert(title: Text("Email or Password is incorrect"))
                     }
-                    .frame(width: 200, height: 40)
-                    .background(RoundedRectangle(cornerRadius: 20).fill(Color(.background)))
+                    .frame(maxWidth: .infinity, minHeight: 50)
+                    .background(RoundedRectangle(cornerRadius: 25).fill(Color.green))
+                    .foregroundStyle(Color.white)
+                    .padding(.horizontal)
             })
             
             
@@ -89,8 +119,10 @@ struct LoginView: View {
                         }
                     }
                 }
-                .frame(width: 200, height: 40)
-                .background(RoundedRectangle(cornerRadius: 20).fill(Color(.background)))
+                .frame(maxWidth: .infinity, minHeight: 50)
+                .background(RoundedRectangle(cornerRadius: 25).fill(.red))
+                .foregroundStyle(Color.white)
+                .padding(.horizontal)
             
             Text("Sign in with Apple")
                 .onTapGesture {
@@ -101,14 +133,10 @@ struct LoginView: View {
                         }
                     }
                 }
-                .frame(width: 200, height: 40)
-                .background(RoundedRectangle(cornerRadius: 20).fill(Color(.background)))
-                
-                    
-
-            
-            
-
+                .frame(maxWidth: .infinity, minHeight: 50)
+                .background(RoundedRectangle(cornerRadius: 25).fill(Color.primary))
+                .foregroundStyle(Color.background)
+                .padding(.horizontal)
         }
         .padding(.all)
         
