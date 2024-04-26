9adfb1ced0bf46b6e790d19a515228144f346e1b
teckenrod <treyeck2@gmail.com>
2024-04-26 11:48:29 -0500

    Added message if no suites found

--- RocketSync/RocketSync/View/Launch/DeviceConnectionView.swift
+++ RocketSync/RocketSync/View/Launch/DeviceConnectionView.swift
@@ -12,22 +12,45 @@ import CoreBluetooth
 struct DeviceConnectionView: View {
     @ObservedObject var bluetoothManager = BluetoothController()
     @State private var selectedPeripheral: CBPeripheral?
+    @State private var peripherals: [CBPeripheral] = []
     var postController: PostsController
     
     var body: some View {
         VStack {
-            List(bluetoothManager.discoveredPeripherals, id: \.identifier) { peripheral in
-                Section {
-                    if peripheral.name == "L-TAS" {
+            if peripherals.isEmpty {
+                Spacer()
+                Text("No L-TAS System Found")
+                    .font(.title)
+                    .padding()
+                    .foregroundColor(.gray)
+                    .multilineTextAlignment(.center)
+                Spacer()
+            } else {
+                List(peripherals, id: \.identifier) { peripheral in
+                    Section {
                         NavigationLink(destination: LaunchDashboardView(bluetoothManager: bluetoothManager, postController: postController, peripheral: peripheral)) {
                             Text(peripheral.name ?? "Unknown")
                                 .bold()
                         }
                     }
+                    .listRowBackground(Color.blue.opacity(0.1))
                 }
-                .listRowBackground(Color.blue.opacity(0.1))
+                .listStyle(GroupedListStyle())
+                .padding(.vertical)
             }
         }
+        .onAppear {
+            getPeripherals()
+        }
+        .refreshable {
+            getPeripherals()
+        }
+    }
+    
+    func getPeripherals() {
+        peripherals = bluetoothManager.discoveredPeripherals.filter({ peripheral in
+            peripheral.name == "L-TAS"
+        })
     }
 }
 
